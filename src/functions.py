d18f70c105b00d269d7c99724ab93b8929b5ba11
Kre0ns <yonpun13@gmail.com>
2025-02-21 15:52:28 +0200

    Change syntax and var names

--- src/functions.py
+++ src/functions.py
@@ -11,38 +11,51 @@ def dice_rolls(num_rolls):
     return rolls
 
 
-def one_to_ten(list_rolls):
-    new_list = []
-    for item in list_rolls:
+def one_to_ten(raw_rolls):
+    parsed_rolls = []
+
+    for item in raw_rolls:
         if item == 1:
-            new_list.append(10)
+            parsed_rolls.append(10)
+
         else:
-            new_list.append(item)
-    return new_list
+            parsed_rolls.append(item)
+
+    return parsed_rolls
 
 
-def points_calc(unsorted_rolls):
-    list_rolls = one_to_ten(unsorted_rolls)
-    counter_dict_rolls = Counter(list_rolls)
+def points_calc(raw_rolls):
+    parsed_rolls = one_to_ten(raw_rolls)
+
+    roll_frequencies = Counter(parsed_rolls)
+    frequency_values = list(roll_frequencies.values())
+
     points = 0
-    num_list_values = list(counter_dict_rolls.values())
 
-    if sorted(list_rolls) == [1, 2, 3, 4, 5, 6]:
+    if sorted(parsed_rolls) == [1, 2, 3, 4, 5, 6]:
         points += 1500
-    elif num_list_values.count(3) == 2:
+
+    elif frequency_values.count(3) == 2:
         points += 2500
-    elif num_list_values.count(2) == 3:
+
+    elif frequency_values.count(2) == 3:
         points += 1500
+
     else:
-        for roll in counter_dict_rolls:
-            if counter_dict_rolls[roll] == 3:
+        for roll in roll_frequencies:
+            if roll_frequencies[roll] == 3:
                 points += roll * 100
-            elif counter_dict_rolls[roll] == 4:
+
+            elif roll_frequencies[roll] == 4:
                 points += 2 * (roll * 100)
-            elif counter_dict_rolls[roll] == 5:
+
+            elif roll_frequencies[roll] == 5:
                 points += 3 * (roll * 100)
-            elif counter_dict_rolls[roll] == 6:
+
+            elif roll_frequencies[roll] == 6:
                 points += 4 * (roll * 100)
+
             elif roll == 10 or roll == 5:
-                points += (roll * 10) * counter_dict_rolls[roll]
+                points += (roll * 10) * roll_frequencies[roll]
+
     return points
