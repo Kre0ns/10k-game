4eec6a04bff9b39939a4613a7374107702ee88ab
Kre0ns <yonpun13@gmail.com>
2025-02-23 14:35:57 +0200

    Add partial functionality for adding players

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import functions
 
 
 def main():
+    player_list = []  # stores the current players
+
     # gets the dice rolls and displays them on screen
     def roll_dice():
         rolls = functions.dice_rolls(6)
@@ -10,8 +12,41 @@ def main():
         for index, dice in enumerate(dice_list):
             dice.configure(text=rolls[index])
 
+    # adds a new player to the player list and creates label for it on screen
     def add_player():
-        pass
+        nonlocal player_list
+        player_name = player_name_entry.get()
+
+        if player_name and len(player_list) < 6:
+            player_list = functions.create_new_player(
+                player_name, player_list
+            )  # adding player to list
+
+            pad_top = 0  # used to pad first widget
+
+            if len(player_frame_list) == 0:
+                pad_top = 10
+
+            # player frame config
+            player_frame = customtkinter.CTkFrame(player_holder_frame, height=250)
+            player_frame.grid_columnconfigure((0), weight=4)
+            player_frame.grid_columnconfigure((1), weight=1)
+            player_frame.grid_rowconfigure((0), weight=1)
+            player_frame.propagate(False)
+            player_frame.grid(
+                column=0,
+                row=len(player_frame_list),
+                padx=10,
+                pady=(pad_top, 10),
+                sticky="ewns",
+            )
+
+            # player label config
+            player_label = customtkinter.CTkLabel(player_frame, text=player_name)
+            player_label.grid(column=0, row=0)
+
+            # adding player frame to player frame list
+            player_frame_list.append(player_frame)
 
     # initializing app
     app = customtkinter.CTk()
@@ -39,8 +74,7 @@ def main():
 
     # player holder frame config (a frame for the player frames)
     player_holder_frame = customtkinter.CTkFrame(app)
-    player_holder_frame.grid_columnconfigure((0), weight=4)
-    player_holder_frame.grid_columnconfigure((1), weight=1)
+    player_holder_frame.grid_columnconfigure((0), weight=1)
     player_holder_frame.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
     player_holder_frame.grid_propagate(False)
     player_frame_list = []  # stores the player frames
